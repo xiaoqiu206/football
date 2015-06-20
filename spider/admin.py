c0a0e278254005eadec09713270438e53a0fb4d3
xiaoqiu206 <xiaoqiu206@163.com>
2015-06-20 13:27:44 +0800

    每页显示300

--- spider/admin.py
+++ spider/admin.py
@@ -7,7 +7,7 @@ class NewsAdmin(ModelAdmin):
     list_display = ('id', 'match_type', 'game_start_time', 'end_score', 'middle_score', 'status', 'team1', 'score',
                     'team2',  'yapan','yapanSB', 'daxiaopan','daxiaopanSB', 'findex', 'create_time')
     ording = ('id',)
-    list_per_page = 50
+    list_per_page = 300
     list_filter = ('create_time',)
     search_fields = ['team1', 'team2', 'findex', 'score']
 
